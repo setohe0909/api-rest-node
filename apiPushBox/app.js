605edf5589b071ed545cf05a33240afadf65348e
seto <zero@seto-zero.(none)>
2015-04-08 15:00:59 -0500

    modificaciondel consumo de restfull y actualizacion con pruebas de carga y almacenamiento de data en mongodb

--- apiPushBox/app.js
+++ apiPushBox/app.js
@@ -1,31 +1,27 @@
-/*Server*/
-var express = require("express"),
-    app = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
-    mongoose = require('mongoose');
+var express         = require("express"),
+    app             = express(),
+    http            = require('http'),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
 
-// Connection To DB
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/data', function(err, res) {
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', 
+  function(err, res) {
     if(err) throw err;
     console.log('Connected to Database');
 });
 
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
 // Import Models and controllers
-var models = require('./models/data')(app, mongoose);
+var models     = require('./models/tvshow')(app, mongoose);
 var TVShowCtrl = require('./controllers/tvshows');
 
-var router = express.Router();
-
-router.get('/', function(req, res) {
-   res.send("Hello World!");
-});
-app.use(router);
 
 // API routes
 var tvshows = express.Router();
@@ -39,9 +35,9 @@ tvshows.route('/tvshows/:id')
   .put(TVShowCtrl.updateTVShow)
   .delete(TVShowCtrl.deleteTVShow);
 
-app.use('/api', tvshows);
+app.use('/push', tvshows);
 
-//Start Server
+// Start server
 app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+  console.log("Not happens anything here. so.. Push again!");
 });
